e6cc8b0c5ee78cf311f0bc46748eade85c3cd502
juna <juna@g.skku.edu>
2021-06-28 06:37:25 +0900

    remove redundant files

--- copy_project.sh
+++
@@ -1,5 +0,0 @@
-result=${PWD##*/}
-echo ${result}_tmp_201123
-
-cd ../
-rsync -av --progress ./$result/ ./${result}_tmp_201123 --exclude result/* --exclude result_important/* --exclude data/* --exclude history/*
--- server_rsync_all_send.sh
+++
@@ -1,7 +0,0 @@
-bash server_rsync54_send.sh &
-# bash server_rsync31_send.sh &
-# bash server_rsync45.sh
-bash server_rsync58_send.sh &
-# bash server_rsync168.sh &
-bash server_rsync61_send.sh &
-bash server_rsync62_send.sh
